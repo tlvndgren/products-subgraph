eddc09eb94ad2a9acf81fe8d4807d664ec4a6bf9
Tyler Lundgren <tylerlundgren@yahoo.com>
2021-12-03 16:53:29 -0800

    Initial product subgraph commit.

---
+++ data/products.js
@@ -0,0 +1,94 @@
+const products = [
+    {
+      "id": 1,
+      "name": "Tres-Zap",
+      "upc": "033579803-9",
+      "priceId": 13
+    },
+    {
+      "id": 2,
+      "name": "Solarbreeze",
+      "upc": "053099130-6",
+      "priceId": 5
+    },
+    {
+      "id": 3,
+      "name": "Transcof",
+      "upc": "262484497-X",
+      "priceId": 11
+    },
+    {
+      "id": 4,
+      "name": "Regrant",
+      "upc": "254050710-7",
+      "priceId": 7
+    },
+    {
+      "id": 5,
+      "name": "Tres-Zap",
+      "upc": "127808039-2",
+      "priceId": 9
+    },
+    {
+      "id": 6,
+      "name": "Zamit",
+      "upc": "790203810-5",
+      "priceId": 10
+    },
+    {
+      "id": 7,
+      "name": "Zamit",
+      "upc": "207136125-3",
+      "priceId": 8
+    },
+    {
+      "id": 8,
+      "name": "Bytecard",
+      "upc": "579168854-7",
+      "priceId": 4
+    },
+    {
+      "id": 9,
+      "name": "Asoka",
+      "upc": "975116899-6",
+      "priceId": 14
+    },
+    {
+      "id": 10,
+      "name": "Sonsing",
+      "upc": "275068244-4",
+      "priceId": 1
+    },
+    {
+      "id": 11,
+      "name": "Voltsillam",
+      "upc": "522110488-1",
+      "priceId": 3
+    },
+    {
+      "id": 12,
+      "name": "Zaam-Dox",
+      "upc": "538778733-4",
+      "priceId": 2
+    },
+    {
+      "id": 13,
+      "name": "Y-find",
+      "upc": "548463305-2",
+      "priceId": 12
+    },
+    {
+      "id": 14,
+      "name": "Y-Solowarm",
+      "upc": "805621224-8",
+      "priceId": 15
+    },
+    {
+      "id": 15,
+      "name": "Pannier",
+      "upc": "013740973-7",
+      "priceId": 6
+    }
+]
+
+module.exports = products;
---
+++ index.js
@@ -0,0 +1,31 @@
+const fs = require("fs");
+const { ApolloServer, gql } = require("apollo-server");
+const { buildSubgraphSchema } = require("@apollo/federation")
+const typeDefs = gql(fs.readFileSync("./products.graphql", 'utf8'));
+const products = require("./data/products.js");
+
+const port = process.env.PORT || 4002
+
+const resolvers = {
+    Product: {
+        __resolverReference(object) {
+            return products.find((product) => product.id === object.id);
+        },
+    },
+    Query: {
+        product(_, { id }) {
+            return product.find((product) => product.id === parseInt(id, 10));
+        },
+        products() {
+            return products
+        }
+    }
+};
+
+const server = new ApolloServer({
+    schema: buildSubgraphSchema([{ typeDefs, resolvers }]),
+  });
+  
+  server.listen({ port }).then(({ url }) => {
+    console.log(`Products service ready at ${url}`);
+  });
