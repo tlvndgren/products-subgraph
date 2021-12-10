c779effa9f0d99cb9972009a84dda18dbfa687b2
Tyler Lundgren <tylerlundgren@yahoo.com>
2021-12-10 09:56:04 -0800

    Building out product attributes a little more.

--- data/products.js
+++ data/products.js
@@ -1,94 +1,139 @@
 const products = [
-    {
-      "id": 1,
-      "name": "Tres-Zap",
-      "upc": "033579803-9",
-      "priceId": 13
-    },
-    {
-      "id": 2,
-      "name": "Solarbreeze",
-      "upc": "053099130-6",
-      "priceId": 5
-    },
-    {
-      "id": 3,
-      "name": "Transcof",
-      "upc": "262484497-X",
-      "priceId": 11
-    },
-    {
-      "id": 4,
-      "name": "Regrant",
-      "upc": "254050710-7",
-      "priceId": 7
-    },
-    {
-      "id": 5,
-      "name": "Tres-Zap",
-      "upc": "127808039-2",
-      "priceId": 9
-    },
-    {
-      "id": 6,
-      "name": "Zamit",
-      "upc": "790203810-5",
-      "priceId": 10
-    },
-    {
-      "id": 7,
-      "name": "Zamit",
-      "upc": "207136125-3",
-      "priceId": 8
-    },
-    {
-      "id": 8,
-      "name": "Bytecard",
-      "upc": "579168854-7",
-      "priceId": 4
-    },
-    {
-      "id": 9,
-      "name": "Asoka",
-      "upc": "975116899-6",
-      "priceId": 14
-    },
-    {
-      "id": 10,
-      "name": "Sonsing",
-      "upc": "275068244-4",
-      "priceId": 1
-    },
-    {
-      "id": 11,
-      "name": "Voltsillam",
-      "upc": "522110488-1",
-      "priceId": 3
-    },
-    {
-      "id": 12,
-      "name": "Zaam-Dox",
-      "upc": "538778733-4",
-      "priceId": 2
-    },
-    {
-      "id": 13,
-      "name": "Y-find",
-      "upc": "548463305-2",
-      "priceId": 12
-    },
-    {
-      "id": 14,
-      "name": "Y-Solowarm",
-      "upc": "805621224-8",
-      "priceId": 15
-    },
-    {
-      "id": 15,
-      "name": "Pannier",
-      "upc": "013740973-7",
-      "priceId": 6
-    }
+  {
+    "id": 1,
+    "name": "Durable Concrete Wallet",
+    "descriptions": "Quam tempore culpa. Quos corrupti harum. Quia fuga accusamus. Quia ab beatae.",
+    "brand": "GoPro",
+    "color": "fuchsia",
+    "upc": "076681074-7",
+    "priceId": 12
+  },
+  {
+    "id": 2,
+    "name": "Awesome Concrete Bottle",
+    "descriptions": "Mollitia magni ab. Sit eum id. Ea atque dolorem. Sit quis minima.",
+    "brand": "LG",
+    "color": "ruby",
+    "upc": "671691309-2",
+    "priceId": 3
+  },
+  {
+    "id": 3,
+    "name": "Rustic Marble Shoes",
+    "descriptions": "Et rerum aut. Eum praesentium dolorem. Quia ipsum nam. Veniam optio qui.",
+    "brand": "Nike",
+    "color": "turquoise",
+    "upc": "202933867-2",
+    "priceId": 11
+  },
+  {
+    "id": 4,
+    "name": "Intelligent Wool Clock",
+    "descriptions": "Aperiam nihil earum. Nihil aut atque. Quis et beatae. Sit qui omnis.",
+    "brand": "LG",
+    "color": "purple",
+    "upc": "360510005-8",
+    "priceId": 2
+  },
+  {
+    "id": 5,
+    "name": "Rustic Steel Lamp",
+    "descriptions": "Corrupti maiores in. Aut dignissimos dolorem. Facilis dolor in. Iusto incidunt nihil.",
+    "brand": "Sony",
+    "color": "violet",
+    "upc": "500696687-4",
+    "priceId": 10
+  },
+  {
+    "id": 6,
+    "name": "Incredible Concrete Shirt",
+    "descriptions": "Maiores dicta temporibus. Officia consequatur omnis. Officia neque maiores. Nesciunt cumque ut.",
+    "brand": "Nikon",
+    "color": "crimson",
+    "upc": "505349938-1",
+    "priceId": 14
+  },
+  {
+    "id": 7,
+    "name": "Mediocre Wool Car",
+    "descriptions": "Dolore sapiente eos. Vero et eius. Sit enim deleniti. Fugit quos quae.",
+    "brand": "Dell",
+    "color": "white",
+    "upc": "861015560-9",
+    "priceId": 13
+  },
+  {
+    "id": 8,
+    "name": "Lightweight Leather Pants",
+    "descriptions": "Facere eveniet alias. Qui et et. Quibusdam quia perferendis. Fuga a odio.",
+    "brand": "Nikon",
+    "color": "amethyst",
+    "upc": "565261619-9",
+    "priceId": 8
+  },
+  {
+    "id": 9,
+    "name": "Lightweight Plastic Chair",
+    "descriptions": "Ducimus repudiandae iste. Reiciendis dolorem dolor. Aut unde nulla. Ut iusto et.",
+    "brand": "Adidas",
+    "color": "orchid",
+    "upc": "572411107-1",
+    "priceId": 6
+  },
+  {
+    "id": 10,
+    "name": "Small Steel Computer",
+    "descriptions": "Ullam sunt voluptas. Mollitia deserunt tenetur. Quas nulla beatae. Doloremque aliquid minus.",
+    "brand": "Apple",
+    "color": "blue",
+    "upc": "221410351-1",
+    "priceId": 15
+  },
+  {
+    "id": 11,
+    "name": "Small Plastic Car",
+    "descriptions": "Voluptatum velit aperiam. Cupiditate et quibusdam. Deleniti ut autem. Provident minus ut.",
+    "brand": "Sony",
+    "color": "raspberry",
+    "upc": "583568928-4",
+    "priceId": 1
+  },
+  {
+    "id": 12,
+    "name": "Awesome Leather Gloves",
+    "descriptions": "Voluptatem molestiae consequatur. Voluptatem hic et. Vel distinctio quasi. Quia blanditiis cupiditate.",
+    "brand": "Dell",
+    "color": "turquoise",
+    "upc": "798971285-7",
+    "priceId": 5
+  },
+  {
+    "id": 13,
+    "name": "Lightweight Bronze Plate",
+    "descriptions": "Facere et esse. Aut vel repudiandae. Nemo fuga rem. Aut ad sit.",
+    "brand": "Dell",
+    "color": "beige",
+    "upc": "258639716-9",
+    "priceId": 4
+  },
+  {
+    "id": 14,
+    "name": "Durable Leather Coat",
+    "descriptions": "Possimus eligendi in. Debitis natus aut. Doloremque minima sit. Quidem rerum consequatur.",
+    "brand": "Nikon",
+    "color": "black",
+    "upc": "026447841-X",
+    "priceId": 9
+  },
+  {
+    "id": 15,
+    "name": "Incredible Linen Table",
+    "descriptions": "Itaque voluptas quas. Omnis et architecto. Dolores et esse. Omnis earum amet.",
+    "brand": "Samsung",
+    "color": "red",
+    "upc": "344349565-6",
+    "priceId": 7
+  }
 ]
 
 module.exports = products;
