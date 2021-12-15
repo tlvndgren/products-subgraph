d0b3fc0cc03eae0c7adb21fed351688b1b893d20
Tyler Lundgren <tylerlundgren@yahoo.com>
2021-12-15 10:04:15 -0800

    Regressing schema for new graph.

--- data/products.js
+++ data/products.js
@@ -5,7 +5,7 @@ const products = [
     "description": "Maxime cum ducimus. Est eos saepe. Et dolorum et. Error esse alias.",
     "brand": "Nike",
     "color": "aquamarine",
-    "upc": "278505971-9",
+    "ean": "278505971-9",
     "priceId": 6,
     "promotionCode": "GoodPromo782676"
   },
@@ -15,7 +15,7 @@ const products = [
     "description": "Natus et accusamus. Voluptas tenetur corrupti. Quas ut et. Ea ut qui.",
     "brand": "Samsung",
     "color": "olive",
-    "upc": "389858924-2",
+    "ean": "389858924-2",
     "priceId": 15,
     "promotionCode": "SpecialCode617496"
   },
@@ -25,7 +25,7 @@ const products = [
     "description": "Consequuntur voluptatem autem. Deleniti modi voluptate. Voluptatum earum recusandae. Adipisci beatae voluptas.",
     "brand": "Adidas",
     "color": "sapphire",
-    "upc": "168932772-3",
+    "ean": "168932772-3",
     "priceId": 13,
     "promotionCode": "AmazingPrice532514"
   },
@@ -35,7 +35,7 @@ const products = [
     "description": "Nesciunt aut aut. Natus nostrum impedit. Accusantium et itaque. Qui et tempora.",
     "brand": "Samsung",
     "color": "peach",
-    "upc": "293589625-4",
+    "ean": "293589625-4",
     "priceId": 5,
     "promotionCode": "KillerPromo857241"
   },
@@ -45,7 +45,7 @@ const products = [
     "description": "Rem beatae voluptas. Tempora laborum unde. Nemo velit sed. Et ipsam ad.",
     "brand": "Apple",
     "color": "orchid",
-    "upc": "756928205-1",
+    "ean": "756928205-1",
     "priceId": 4,
     "promotionCode": "KillerDiscount836337"
   },
@@ -55,7 +55,7 @@ const products = [
     "description": "Nisi et harum. Aperiam placeat nemo. Rerum et nostrum. Doloribus perspiciatis modi.",
     "brand": "Adidas",
     "color": "byzantium",
-    "upc": "270363602-4",
+    "ean": "270363602-4",
     "priceId": 8,
     "promotionCode": "GoodDeal101538"
   },
@@ -65,7 +65,7 @@ const products = [
     "description": "Facilis tempore perferendis. Sed ut ipsam. Esse pariatur molestias. Odit illo qui.",
     "brand": "Samsung",
     "color": "champagne",
-    "upc": "999706664-2",
+    "ean": "999706664-2",
     "priceId": 3,
     "promotionCode": "IncredibleDeal311633"
   },
@@ -75,7 +75,7 @@ const products = [
     "description": "Odio exercitationem quod. Aut consequatur eum. Ut est ea. Sapiente eos officiis.",
     "brand": "Beats",
     "color": "crimson",
-    "upc": "031824969-3",
+    "ean": "031824969-3",
     "priceId": 14,
     "promotionCode": "KillerPromotion742742"
   },
@@ -85,7 +85,7 @@ const products = [
     "description": "Laborum temporibus rem. Fugit repellat quia. Id et doloremque. Et eaque ut.",
     "brand": "Dell",
     "color": "plum",
-    "upc": "749173098-1",
+    "ean": "749173098-1",
     "priceId": 11,
     "promotionCode": "KillerDiscount482400"
   },
@@ -95,7 +95,7 @@ const products = [
     "description": "Ad aut sed. Ullam vero inventore. Dolor aliquid vel. Ad quia voluptatem.",
     "brand": "Samsung",
     "color": "gray",
-    "upc": "112836746-7",
+    "ean": "112836746-7",
     "priceId": 1,
     "promotionCode": "KillerDeal439325"
   },
@@ -105,7 +105,7 @@ const products = [
     "description": "Aliquid molestias minima. Ut distinctio facilis. Quia iure deleniti. Sit ab nobis.",
     "brand": "Beats",
     "color": "orange",
-    "upc": "479196554-X",
+    "ean": "479196554-X",
     "priceId": 2,
     "promotionCode": "StellarSale575433"
   },
@@ -115,7 +115,7 @@ const products = [
     "description": "Cupiditate ipsa fugiat. Voluptatibus voluptatem deserunt. Quam nesciunt beatae. Sint nostrum eos.",
     "brand": "Sony",
     "color": "silver",
-    "upc": "343971106-4",
+    "ean": "343971106-4",
     "priceId": 9,
     "promotionCode": "IncrediblePromo174674"
   },
@@ -125,7 +125,7 @@ const products = [
     "description": "Neque earum rerum. Fuga odit ut. Et ut consequuntur. Qui fugit fugiat.",
     "brand": "Beats",
     "color": "chocolate",
-    "upc": "049439977-5",
+    "ean": "049439977-5",
     "priceId": 10,
     "promotionCode": "StellarSale393003"
   },
@@ -135,7 +135,7 @@ const products = [
     "description": "Eum aut consequatur. Quia veritatis qui. Assumenda modi ipsam. Quibusdam eum provident.",
     "brand": "GoPro",
     "color": "chocolate",
-    "upc": "822535223-8",
+    "ean": "822535223-8",
     "priceId": 7,
     "promotionCode": "CoolPromotion901262"
   },
@@ -145,7 +145,7 @@ const products = [
     "description": "Labore dolor magnam. Tenetur ab cumque. Suscipit aut dolore. Aut ipsum nihil.",
     "brand": "Adidas",
     "color": "lemon",
-    "upc": "442002296-5",
+    "ean": "442002296-5",
     "priceId": 12,
     "promotionCode": "SpecialPrice245300"
   }
